69888d7f36393c026a520e7bc350a5537c16fa4a
花亮 <xiufeng.zxf@alibaba-inc.com>
2016-08-17 14:00:31 +0800

    sd

---
+++ dsfds/dsfds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dsfds
+//
+//  Created by mulala on 16/8/17.
+//  Copyright © 2016年 mulala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dsfds/dsfds/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  dsfds
+//
+//  Created by mulala on 16/8/17.
+//  Copyright © 2016年 mulala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
