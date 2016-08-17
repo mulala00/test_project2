9af28b1e34146f70309bfc5317f68a69d080b5e9
花亮 <xiufeng.zxf@alibaba-inc.com>
2016-08-17 13:36:59 +0800

    auto

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by mulala on 16/8/17.
+//  Copyright © 2016年 mulala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  test
+//
+//  Created by mulala on 16/8/17.
+//  Copyright © 2016年 mulala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
